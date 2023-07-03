c518b1fd24d502bf56727a4736787a866b8d21b5
Hoo <adobephotoshop@naver.com>
2023-07-03 09:56:01 +0900

    errorfix

--- src/main/java/com/multi/quizwiki/problem/service/ProblemService.java
+++ src/main/java/com/multi/quizwiki/problem/service/ProblemService.java
@@ -3,11 +3,12 @@ package com.multi.quizwiki.problem.service;
 import java.util.List;
 
 import com.multi.quizwiki.dto.ProblemDTO;
+import com.multi.quizwiki.problem.entity.ProblemCateEntity;
 import com.multi.quizwiki.problem.entity.ProblemEntity;
 
 public interface ProblemService {
 	public List<ProblemDTO> problem_findLikedByMemberId(String memberId, int size , int page);
 	public List<ProblemDTO> problem_findOrderByInquiry(int limit);
-	
 	public ProblemEntity problem_findByProblemId(int problemId);
+	public List<ProblemCateEntity> problemCate_findAll();
 }
