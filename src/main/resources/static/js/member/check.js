abe97e2cd25367c06d6e317e0a76e188b8c13b03
KHaeWon <hyen1616@192.168.219.106>
2023-07-04 16:12:07 +0900

    sms 인증 추가, 회원가입 시 mkt_opt 값 오류 수정

--- src/main/resources/static/js/member/check.js
+++ src/main/resources/static/js/member/check.js
@@ -1,4 +1,54 @@
+
+// member_mkt_opt 값 설정하기
+function updateMemberMktOpt() {
+	  var mktOpt = "";
+	  var selectAllCheckbox = document.getElementById("selectall");
+	  var smsCheckbox = document.getElementById("aggrement_sms");
+	  var emailCheckbox = document.getElementById("aggrement_email");
+
+	  if (selectAllCheckbox.checked) {
+	    mktOpt = "마케팅 수신 전체 동의";
+	  } else {
+	    if (smsCheckbox.checked) {
+	      mktOpt = "sms";
+	    }
+	    if (emailCheckbox.checked) {
+	      mktOpt = "email";
+	    }
+	  }
+	  console.log("mktOpt: " + mktOpt);
+}
+
+// 체크박스
 function checkSelectAll()  {
+  // 전체 체크박스
+  const checkboxes 
+    = document.querySelectorAll('.mkt_opt_sub');
+  // 선택된 체크박스
+  const checked 
+    = document.querySelectorAll('.mkt_opt_sub:checked');
+  // select all 체크박스
+  const selectAll 
+    = document.querySelector('input[name="member_mkt_opt"]');
+  
+  if(checkboxes.length === checked.length)  {
+    selectAll.checked = true;
+  }else {
+    selectAll.checked = false;
+  }
+  updateMemberMktOpt();
+}
+
+function selectAll(selectAll)  {
+	const checkboxes = document.querySelectorAll('.mkt_opt_sub');
+  
+  checkboxes.forEach((checkbox) => {
+    checkbox.checked = selectAll.checked
+  })
+  updateMemberMktOpt();
+}
+
+/*function checkSelectAll()  {
   // 전체 체크박스
   const checkboxes 
     = document.querySelectorAll('input[name="mkt_opt_sub"]');
@@ -24,4 +74,4 @@ function selectAll(selectAll)  {
   checkboxes.forEach((checkbox) => {
     checkbox.checked = selectAll.checked
   })
-}
+}*/
